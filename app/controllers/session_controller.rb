595a1af57865eb50ed476fb6c18a9737185770ad
dmitrbayan <dmitrybayan@yandex.ru>
2020-01-13 16:40:58 +0300

    some bugfixes

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,18 +1,20 @@
-class SessionController < ApplicationController
-	def create
-		@user = User.from_omniauth(request.env['omniauth.auth'])
-		session[:user_id] = @user.id
-		if @user != nil
-			flash[:success] = "Добро пожаловать на сервис, #{@user.first_name}!"
-			redirect_to view_path
-		else
-			flash[:warning] = "Упс, не удалось войти :("
-		end
-	end
+# frozen_string_literal: true
 
-	def destroy
-    	session.delete(:user_id)
-    	@current_user = nil
-    	redirect_to root_path
+class SessionController < ApplicationController
+  def create
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+    session[:user_id] = @user.id
+    if !@user.nil?
+      flash[:success] = "Welcome, #{@user.first_name}!"
+      redirect_to view_path
+    else
+      flash[:warning] = 'Unknown error'
     end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    @current_user = nil
+    redirect_to root_path
+  end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :vkontakte, '7251431', 'z8WHZnOy4G1FvuUq9Lb0'
-  provider :facebook, '448070755873557', '62eb16f667c965f266de63b72da68f39'
+  provider :vkontakte, ENV["VK_KEY"], ENV["VK_SECRET"]
+  provider :facebook, ENV["FB_KEY"], ENV["FB_SECRET"]
 end
