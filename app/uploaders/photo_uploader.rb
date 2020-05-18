1d83b6802a2313ac1414d0417f95f38549b51615
dmitrbayan <dmitrybayan@yandex.ru>
2020-05-18 13:54:31 +0300

    config carrierwave

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -7,7 +7,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
   process resize_to_limit: [400, 400]
 
   if Rails.env.production?
-    storage :fog
+    storage :aws
   else
     storage :file
   end
